da8dd06dfd0d48c629cc092f9cc9284e85582dd8
GeneralOfAR <73152734+GeneralOfAR@users.noreply.github.com>
2023-04-29 22:13:30 -0600

    Update HardcoreBypass.lua

--- HardcoreBypass.lua
+++ HardcoreBypass.lua
@@ -1,19 +1,1 @@
---some guy requested was originally found (to my understanding) by scarz ofc knowing how to do it its easy to recreate something so simple
 
-game:GetService("Players").GeneralofAR.PlayerGui.MapSelection.Frame.Launcher.Hardcore.Description.Purchase.Visible = false
-
-game:GetService("Players").GeneralofAR.PlayerGui.MapSelection.Frame.Launcher.Hardcore.Description.Play.Visible = true
-
--- yes it was controlled by a simple gui,
-
-
-
---[[
-
-HOW TO USE:
-
-run this script while in lobby and have hardcore selected then press play
-
-(if we ever get a cheap bypass to roblox anti cheat at least)
-
-]]
